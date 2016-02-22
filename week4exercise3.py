92414f041a195b5e6fd989183e3dcf9da629e0e1
Matt Miller <mattmiller87@gmail.com>
2016-02-22 10:29:33 -0800

    Update week4exercise3.py

--- week4exercise3.py
+++ week4exercise3.py
@@ -17,10 +17,22 @@ int('5 years') generates a ValueError exception.
 Print the dictionary to standard output.
 '''
 
+import re
+
 uptime1 = 'twb-sf-881 uptime is 6 weeks, 4 days, 2 hours, 25 minutes'
 uptime2 = '3750RJ uptime is 1 hour, 29 minutes'
 uptime3 = 'CATS3560 uptime is 8 weeks, 4 days, 18 hours, 16 minutes'
 uptime4 = 'rtr1 uptime is 5 years, 18 weeks, 8 hours, 23 minutes'
+
+list_of_uptime = [uptime1, uptime2, uptime3, uptime4]
+
+for uptime in list_of_uptime: #loop through list_of_uptime
+	split_device_and_time = uptime.split(" uptime is ")
+	hostname,time_with_words = split_device_and_time
+	print hostname + ": " + time_with_words
+
+
+'''
 
 #from before
 import re
@@ -44,3 +56,5 @@ for item in list_of_entries: #loop through list_of_entries
 		'as_path': split_entry[3] #[3] = 0 701 9505 17408 2.1465 i
 	}
 	print "%-20s %-20s" % (dict['ip_prefix'],dict['as_path']) #table information printed
+
+'''
