6728df999c08479925812e7aebd33a244d588731
Matt Miller <mattmiller87@gmail.com>
2016-03-04 19:26:39 -0800

    some exercises completed

--- week6exercise5.py
+++ week6exercise5.py
@@ -1,4 +1,7 @@
 '''
-5. Write a program that prompts a user for an IP address, then checks if the IP address is valid, and then converts the IP address to binary (dotted decimal format). Re-use the functions created in exercises 3 and 4 ('import' the functions into your new program).
+5. Write a program that prompts a user for an IP address, then checks if the IP address is valid,
+ and then converts the IP address to binary (dotted decimal format). 
+ Re-use the functions created in exercises 3 and 4 ('import' the functions into your new program).
 
 '''
+
